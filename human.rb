52f88f1b1c6c89b61a81742fb4d18722ec2d47a7
kazuya.haga <hg.future.160@icloud.com>
2021-08-14 11:33:49 +0000

    first

---
+++ human.rb
@@ -0,0 +1,16 @@
+require './thinkable.rb'
+require './animal.rb'
+
+class Human < Animal
+    
+    include Thinkable
+    
+    attr_accessor :hobby
+    
+    def initialize (name,age,hobby)
+        super(name,age)
+        self.hobby = hobby
+    end
+    
+end
+
